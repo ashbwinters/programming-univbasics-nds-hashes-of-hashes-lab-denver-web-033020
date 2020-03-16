4663bf7c0256220db5a3e7053b3951723b96fb16
Ashley Winters <ashbwinters@gmail.com>
2020-03-16 22:25:36 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -44,8 +44,7 @@ def naming_system
             sub_category: {
               label: "Genus",
               sub_category: {
-                label: "Species",
-                sub_category: {}
+                label: "Species"
               }
             }
           }
